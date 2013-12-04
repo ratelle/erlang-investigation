8e2ce689aca8618c96ee188361723f344a488ada
Jeremie Lasalle Ratelle <jeremie.ratelle@gmail.com>
2013-12-04 16:38:40 -0500

    Add some stuff for spinning

--- reductions/measure.erl
+++ reductions/measure.erl
@@ -1,10 +1,10 @@
 -module(measure).
 
--export([measure_direct/4, measure_external/3, measure_external_n/3, measure_function_n/2]).
+-export([measure_direct/5, measure_external/3, measure_external_n/4, measure_function_n/2]).
 
-measure_direct(Module, Function, N, Percent) ->
-    {TimeSpin, _} = measure_spin(N * Percent div 100),
-    {TimeCity, _} = measure_external(Module, Function, N),
+measure_direct(Module, Function, Args, N, Percent) ->
+    {TimeSpin, _} = measure_external_n(spin, spin, [2000], N * Percent div 100),
+    {TimeCity, _} = measure_external_n(Module, Function, Args, N),
     TimeCity / TimeSpin.
 
 measure_external(Module, Function, N) ->
@@ -12,9 +12,9 @@ measure_external(Module, Function, N) ->
     {Time,_} = timer:tc(Module, Function, [N]),
     {Time, Time/N}.
 
-measure_external_n(Module, Function, N) ->
+measure_external_n(Module, Function, Args, N) ->
     [erlang:garbage_collect(Pid) || Pid <- processes()],
-    {Time,_} = timer:tc(fun run_external_n/3, [Module,Function,N]),
+    {Time,_} = timer:tc(fun run_external_n/4, [Module,Function,Args,N]),
     {Time, Time/N}.
 
 measure_function_n(Func, N) ->
@@ -22,11 +22,11 @@ measure_function_n(Func, N) ->
     {Time,_} = timer:tc(fun run_function_n/2, [Func,N]),
     {Time, Time/N}.
 
-run_external_n(_Module, _Function, 0) ->
+run_external_n(_Module, _Function, _Args, 0) ->
     ok;
-run_external_n(Module, Function, N) ->
-    Module:Function(),
-    run_external_n(Module, Function, N-1).
+run_external_n(Module, Function, Args, N) ->
+    erlang:apply(Module, Function, Args),
+    run_external_n(Module, Function, Args, N-1).
 
 run_function_n(_Func, 0) ->
     ok;
@@ -36,16 +36,5 @@ run_function_n(Func, N) ->
 
 measure_spin(N) ->
     [erlang:garbage_collect(Pid) || Pid <- processes()],
-    {Time,_} = timer:tc(fun spin_n/1, [N]),
+    {Time,_} = timer:tc(fun spin:spin_n/1, [N]),
     {Time, Time/N}.
-
-spin_n(0) ->
-    ok;
-spin_n(N) ->
-    spin(2000),
-    spin_n(N-1).
-
-spin(0) ->
-    ok;
-spin(N) ->
-    spin(N-1).
